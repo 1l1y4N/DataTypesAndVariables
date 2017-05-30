1080b66a19dfc3a3263c26b9bc897f2cf795565d
1l1y4N <cvetkov.iliyan@gmail.com>
2017-05-30 23:15:09 +0300

    4 done

---
+++ P4Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace P4Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ppl = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)ppl / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
