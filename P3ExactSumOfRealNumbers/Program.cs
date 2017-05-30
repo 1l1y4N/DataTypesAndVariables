b38aaeb893d1f4ed22cffaa033cb242f887edd95
1l1y4N <cvetkov.iliyan@gmail.com>
2017-05-30 22:51:35 +0300

    3 done

---
+++ P3ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace P3ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = new decimal (0);
+            for (int i = 0; i < n; i++)
+            {
+                decimal d = decimal.Parse(Console.ReadLine());
+                sum = sum + d;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
