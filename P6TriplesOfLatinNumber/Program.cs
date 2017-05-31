26da638fe5f9fec90b80a805b4f814a8022d4956
1l1y4N <cvetkov.iliyan@gmail.com>
2017-05-31 20:32:27 +0300

    All done. Ones missing are already solved

---
+++ P6TriplesOfLatinNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace P6TriplesOfLatinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            char letter = (char)('a' + range);
+            for (char i = 'a'; i < letter; i++)
+            {
+                for (char j = 'a'; j < letter; j++)
+                {
+                    for (char k = 'a'; k < letter; k++)
+                    {
+                        Console.Write(i);
+                        Console.Write(j);
+                        Console.Write(k);
+                        Console.Write("\r\n");
+                    }
+              
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ P9RefractorSpecialNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9RefractorSpecialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            bool isSpecial = false;
+            for (int num = 1; num <= totalNumber; num++)
+            {
+                int currentNumber = num;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{currentNumber} -> {isSpecial}");
+                sum = 0;
+                num = currentNumber;
+            }
+        }
+    }
+}
