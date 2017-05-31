a3f6dd16ea41102298d1f0061a917df4df2afdd8
1l1y4N <cvetkov.iliyan@gmail.com>
2017-05-31 16:04:43 +0300

    6 done

---
+++ P6SpecialNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6SpecialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <=n; i++)
+            {
+                int result = i.ToString().Sum(c => c - '0');
+                
+                if (result==5)
+                {
+                    Console.WriteLine("{0}->True",i);
+                } else if (result == 7)
+                {
+                    Console.WriteLine("{0}->True", i);
+                }
+                else if (result == 11)
+                {
+                    Console.WriteLine("{0}->True", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0}->False", i);
+                }
+            }
+        }
+    }
+}
