1d83c24cf749ad3c818c875a9d033ce0b605acf8
1l1y4N <cvetkov.iliyan@gmail.com>
2017-05-31 19:45:45 +0300

    7 done

---
+++ P7Greeting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace P7Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var surname = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Hello, {name} {surname}. You are {age} years old.");
+        }
+    }
+}
